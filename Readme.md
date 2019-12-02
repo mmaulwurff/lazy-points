d4133f87364e5b642ba10c9541a41d71737552c4
Alexander Kromm <mmaulwurff@gmail.com>
2019-12-02 23:35:17 +0400

    add base files

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Scoring for GZDoom
+
+Summary: do things, get points!
+
+## Acknowledgments
+
+### Credits
+
+- Scoring system is based on [ScoreDoom](https://zdoom.org/wiki/ScoreDoom)
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+name=zcore
+version=$(git describe --abbrev=0 --tags)
+filename=$name-$version.pk3
+
+rm -f $filename
+
+zip $filename    \
+    zscript/*.zs \
+    *.md  \
+    *.txt \
+    *.zs  \
+
+gzdoom -iwad $IWAD     \
+       -file $filename \
+       "$1" "$2" "$3"  \
+       +map test
